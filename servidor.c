415b41a93ecb953efee63b3a2702771bdcfca3da
jorgealexis07 <78820449+jorgealexis07@users.noreply.github.com>
2021-07-18 13:48:08 -0500

    Update servidor.c

--- servidor.c
+++ servidor.c
@@ -5,63 +5,61 @@
 #include <netinet/in.h>
 #include <string.h>
  
-//FunciÃƒÂ³n principal
+//Funcion principal
 int main(int argc, char **argv)
 {
  
 if(argc > 1)
-{
+{ 
+  //Primer paso, definir variables
+  int fd,fd2,longitud_cliente,puerto;
+  puerto=atoi(argv[1]);
  
-//Primer paso, definir variables
-int fd,fd2,longitud_cliente,puerto;
-puerto=atoi(argv[1]);
+  //Se necesitan dos estructuras del tipo sockaddr
+  //La primera guarda info del server
+  //La segunda del cliente
+   struct sockaddr_in server;
+   struct sockaddr_in client;
  
-//Se necesitan dos estructuras del tipo sockaddr
-//La primera guarda info del server
-//La segunda del cliente
- struct sockaddr_in server;
- struct sockaddr_in client;
+  //Configuracion del servidor
+  server.sin_family= AF_INET; //Familia TCP/IP
+  server.sin_port = htons(puerto); //Puerto
+  server.sin_addr.s_addr = INADDR_ANY; //Cualquier cliente puede conectarse
+  bzero(&(server.sin_zero),8); //Funcion que rellena con 0's
  
-//Configuracion del servidor
- server.sin_family= AF_INET; //Familia TCP/IP
- server.sin_port = htons(puerto); //Puerto
- server.sin_addr.s_addr = INADDR_ANY; //Cualquier cliente puede conectarse
- bzero(&(server.sin_zero),8); //Funcion que rellena con 0's
- 
- //Paso 2, definicion de socket
- if (( fd=socket(AF_INET,SOCK_STREAM,0) )<0){
- perror("Error de apertura de socket");
- exit(-1);
- }
+  //Paso 2, definicion de socket
+  if (( fd=socket(AF_INET,SOCK_STREAM,0) )<0){
+     perror("Error de apertura de socket");
+     exit(-1);
+   }
  
  //Paso 3, avisar al sistema que se creo un socket
  if(bind(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) {
- printf("error en bind() \n");
- exit(-1);
+   printf("error en bind() \n");
+   exit(-1);
  }
  
- //Paso 4, establecer el socket en modo escucha
-if(listen(fd,5) == -1) {
- printf("error en listen()\n");
- exit(-1);
+  //Paso 4, establecer el socket en modo escucha
+ if(listen(fd,5) == -1) {
+  printf("error en listen()\n");
+  exit(-1);
  }
  
  //Paso5, aceptar conexiones
  while(1) {
- longitud_cliente= sizeof(struct sockaddr_in);
- /* A continuaciÃƒÂ³n la llamada a accept() */
+   longitud_cliente= sizeof(struct sockaddr_in);
+   /* A continuacion la llamada a accept() */
  if ((fd2 = accept(fd,(struct sockaddr *)&client,&longitud_cliente))==-1) {
- printf("error en accept()\n");
- exit(-1);
+   printf("error en accept()\n");
+   exit(-1);
  }
  //printf("Direccion ip del cliente: %s\n",fd);
  //printf("Puerto del cliente: %s\n",puerto);
  send(fd2,"Bienvenido a mi servidor.\n",26,0);
- 
-close(fd2); /* cierra fd2 */
+ close(fd2); /* cierra fd2 */
  }
 close(fd);
 }
 else{
-printf("NO se ingreso el puerto por parametro\n");
+  printf("NO se ingreso el puerto por parametro\n");
 }
